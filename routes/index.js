b88994d9d1c32e5d96cb272dad22c53ada6c7b60
Dominic  Smith <dominicsmith@Dominics-MacBook-Pro.local>
2020-09-01 19:02:36 -0400

    df

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/lyri-sept-2020', function(req, res, next) {
+  res.render('lyri-sept-2020', { title: 'Express' });
+});
+
+router.get('/atelo-sept-2020', function(req, res, next) {
+  res.render('atelo-sept-2020', { title: 'Express' });
+});
+
+router.get('/aiden-craig-sept-2020', function(req, res, next) {
+  res.render('aiden-craig-sept-2020', { title: 'Express' });
+});
+
 module.exports = router;
